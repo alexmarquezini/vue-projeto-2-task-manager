428803fbcb5fb214618d61da1f64a0714ae95181
Alex Marquezini <alexsmarquez@hotmail.com>
2022-02-19 15:15:44 -0300

    Project ended

--- src/components/TaskGrid.vue
+++ src/components/TaskGrid.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="task-grid">
     <template v-if="tasks.length">
-      <Task v-for="task in tasks" :key="task.name" :task="task" />
+      <Task v-for="(task, i) in tasks" :key="task.name" :task="task" 
+      @taskDeleted="$emit('taskDeleted', i)"
+      @taskStateChanged="$emit('taskStateChanged', i)"/>
     </template>
     <p v-else class="no-task">Nada pra fazer! 😊</p>
   </div>
